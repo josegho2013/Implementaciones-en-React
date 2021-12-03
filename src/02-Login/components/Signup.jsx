965a4f7d62ff3794cbfdddf1a6aba3db96255d15
jose hernandez <josegho2013@gmail.com>
2021-12-02 21:59:13 -0300

    .

--- src/02-Login/components/Signup.jsx
+++ src/02-Login/components/Signup.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../css/login.css";
 
 import { FcGoogle } from "react-icons/fc";
@@ -8,34 +9,50 @@ const Login = () => {
     return (
       <div>
         <form className="form">
-          <h1>Login</h1>
+          <h1>Signup</h1>
           <div>
             <div>
-              <label> Email</label>
+              <label>User Name</label>
             </div>
             <input className="input" type="text" />
           </div>
           <div>
-            <label> Password</label>
+            <label> Email</label> 
           </div>
           <div>
             <input type="password" className="input" />
           </div>
           <div>
+            <div>
+              <label>Password </label>
+            </div>
+            <input className="input" type="text" />
+          </div>
+          <div>
+            <label> Confirm Password</label>
+          </div>
+          <div>
+            <input type="password" className="input" />
+          </div>
+          <div>
+            
           
             <button className="button-add">Login</button>
             
           </div>
-  
+          <Link to="/Login">
+          <button >Home</button>
+          </Link>
           <div>
             <p>Start Section With</p>
           </div>
           <div className="icon">
+            <Link to="/google.com">
             <button>
               {" "}
               <FcGoogle className="google" />{" "}
             </button>
-  
+           </Link>
             <button>
               {" "}
               <AiFillFacebook className="face" />
