815f4946d189840ebfccf5b5639660f26e850cc8
jose hernandez <josegho2013@gmail.com>
2022-01-26 20:47:43 -0300

    update

--- src/04-Consumo de Api/components/Search.jsx
+++ src/04-Consumo de Api/components/Search.jsx
@@ -1,22 +1,45 @@
-import React from "react";
+import { React, useState } from "react";
 import { useEffect } from "react";
 const axios = require("axios");
-const { API_KEY } = process.env;
+const API_KEY = "25429544-c8c9eb5d0b1ad0c7612e529d6"
+// const { API_KEY } = process.env;
 
 const Search = () => {
-  const getPixabay = async () => {
-    const apiUrl = await axios.get(`https://pixabay.com/api/?=${API_KEY}`);
+  const getPixabay = async (payload) => {
+    const apiUrl = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${payload}`);
+    console.log(API_KEY, "API_KEY");
     console.log(apiUrl, "apiUrl");
+    
+
+
+
+  };
+  useEffect(() => {
+    
+  }, []);
+
+  const [search, setSearch] = useState("");
+ 
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    getPixabay(search)
+   
   };
 
 
-  useEffect(() => {
-    getPixabay();
-  }, []);
-
   return (
     <div>
-      <h1>hola soy la busqueda</h1>
+      <form onSubmit={(e) => handleSearch(e)} >
+      <input
+          type="search"
+          placeholder="search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">Search</button>
+
+      </form>
     </div>
   );
 };
