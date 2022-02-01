44f7bb8a76726ae822d02660e89bafbdcfe6e1db
jose hernandez <josegho2013@gmail.com>
2022-02-01 18:55:36 -0300

    modal

--- src/03-Crud/components/Crud.jsx
+++ src/03-Crud/components/Crud.jsx
@@ -1,10 +1,186 @@
 import React from "react";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+import {
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  ModalFooter,
+} from "reactstrap";
+// import "../css/styles.css"
+
+const data = [
+  { id: 1, personaje: "Naruto", anime: "Naruto" },
+  { id: 2, personaje: "Goku", anime: "Dragon Ball" },
+  { id: 3, personaje: "Kenshin Himura", anime: "Rurouni Kenshin" },
+  { id: 4, personaje: "Monkey D. Luffy", anime: "One Piece" },
+  {id: 5,personaje: "Edward Elric",anime: "Fullmetal Alchemist: Brotherhood",},
+  { id: 6, personaje: "Seto Kaiba", anime: "Yu-Gi-Oh!" },
+];
 
 const Crud = () => {
-    console.log("hola")
+  // const[data, setData]=useState
+
   return (
     <div>
-      <h1>hola soy crud</h1>
+      <Container>
+        <br />
+        <Button color="success">Insertar Nuevo Personaje</Button>
+        <br />
+        <br />
+        <Table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Personaje</th>
+              <th>Anime</th>
+              <th>Acción</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.personaje}</td>
+                <td>{item.anime}</td>
+
+                <td>
+                  <Button color="primary">Editar</Button>{" "}
+                  <Button color="danger">Eliminar</Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+
+      {/* <Modal isOpen={this.state.modalActualizar}>
+          <ModalHeader>
+           <div><h3>Editar Registro</h3></div>
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>
+               Id:
+              </label>
+            
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.form.id}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Personaje: 
+              </label>
+              <input
+                className="form-control"
+                name="personaje"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.personaje}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Anime: 
+              </label>
+              <input
+                className="form-control"
+                name="anime"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.anime}
+              />
+            </FormGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.editar(this.state.form)}
+            >
+              Editar
+            </Button>
+            <Button
+              color="danger"
+              onClick={() => this.cerrarModalActualizar()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
+
+
+
+        <Modal isOpen={this.state.modalInsertar}>
+          <ModalHeader>
+           <div><h3>Insertar Personaje</h3></div>
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>
+                Id: 
+              </label>
+              
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.data.length+1}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Personaje: 
+              </label>
+              <input
+                className="form-control"
+                name="personaje"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Anime: 
+              </label>
+              <input
+                className="form-control"
+                name="anime"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.insertar()}
+            >
+              Insertar
+            </Button>
+            <Button
+              className="btn btn-danger"
+              onClick={() => this.cerrarModalInsertar()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal> */}
     </div>
   );
 };
--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Signup from "./02-Login/components/Signup"
 import Crud from "./03-Crud/components/Crud"
 import Navbar from "./04-Consumo de Api/components/Navbar";
 import Quizz from "./05-Quizz-React/componenets/Quizz"
+import Modal from "./06-Modal/components/Modal"
 import "./App.css";
 
 function App() {
@@ -16,6 +17,8 @@ function App() {
       <Route exact path="/crud" component={Crud} />
       <Route exact path="/Signup" component={Signup} />
       <Route exact path="/quizz" component={Quizz} />
+      <Route exact path="/modal" component={Modal} />
+
 
 
       
