b20f76414ece012310b0fa388dfecf44396c6705
jose hernandez <josegho2013@gmail.com>
2022-02-23 11:34:38 -0300

    update

--- src/03-Crud/components/Crud.jsx
+++ src/03-Crud/components/Crud.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { connect } from "../database/db";
+// import { connect } from "../database/db";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
   Table,
@@ -14,18 +14,18 @@ import {
 } from "reactstrap";
 // import "../css/styles.css"
 
-const palo = [
-  { id: 1, personaje: "Naruto", anime: "Naruto" },
-  { id: 2, personaje: "Goku", anime: "Dragon Ball" },
-  { id: 3, personaje: "Kenshin Himura", anime: "Rurouni Kenshin" },
-  { id: 4, personaje: "Monkey D. Luffy", anime: "One Piece" },
-  {
-    id: 5,
-    personaje: "Edward Elric",
-    anime: "Fullmetal Alchemist: Brotherhood",
-  },
-  { id: 6, personaje: "Seto Kaiba", anime: "Yu-Gi-Oh!" },
-];
+// const palo = [
+//   { id: 1, personaje: "Naruto", anime: "Naruto" },
+//   { id: 2, personaje: "Goku", anime: "Dragon Ball" },
+//   { id: 3, personaje: "Kenshin Himura", anime: "Rurouni Kenshin" },
+//   { id: 4, personaje: "Monkey D. Luffy", anime: "One Piece" },
+//   {
+//     id: 5,
+//     personaje: "Edward Elric",
+//     anime: "Fullmetal Alchemist: Brotherhood",
+//   },
+//   { id: 6, personaje: "Seto Kaiba", anime: "Yu-Gi-Oh!" },
+// ];
 // console.log("01INFO", info)
 
 const Crud = () => {
@@ -259,3 +259,28 @@ const Crud = () => {
 };
 
 export default Crud;
+
+
+
+
+// [
+//   {"personaje": "Naruto",
+//   "anime": "Naruto" },
+  
+//   { "personaje": "Goku", 
+//   "anime": "Dragon Ball" },
+  
+//   { "personaje": "Kenshin Himura",
+//   "anime": "Rurouni Kenshin" },
+  
+//   {  "personaje": "Monkey D. Luffy", 
+//   "anime": "One Piece" },
+  
+    
+//     {"personaje": "Edward Elric",
+//     "anime": "Fullmetal Alchemist: Brotherhood",
+//   },
+  
+//   { "personaje": "Seto Kaiba",
+//    "anime": "Yu-Gi-Oh!" }
+// ]
